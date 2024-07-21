c714aac4ebc7243f93829cf546d93bbba6ffb1ad
Jérôme Abel <contact@jeromeabel.net>
2024-07-21 20:27:36 +0200

    feat: add filter by options

--- composables/useProducts.ts
+++ composables/useProducts.ts
@@ -4,13 +4,18 @@ export const useProducts = (initialProducts: Product[]) => {
 
     const products = ref(initialProducts)
     const searchTerm = ref('')
+    const options = ref<string[]>([])
 
     const updateSearchTerm = (newTerm: string) => searchTerm.value = newTerm
+    const updateOptions = (newOptions: string[]) => options.value = [...newOptions]
 
     const filteredProducts = computed(() => {
         let filtered = products.value
         if(searchTerm.value) {
             filtered = products.value.filter(product => product.name.toLowerCase().includes(searchTerm.value.toLowerCase()))
+        }
+        if(options.value.length > 0) {
+            filtered = filtered.filter(product => product.options.some(option => options.value.includes(option)))
         }
         return filtered;
         }
@@ -19,6 +24,7 @@ export const useProducts = (initialProducts: Product[]) => {
     return {
         filteredProducts,
         searchTerm,
+        updateOptions,
         updateSearchTerm,
     }
 }
--- tests/useProducts.spec.ts
+++ tests/useProducts.spec.ts
@@ -8,9 +8,9 @@ describe("useProducts", () => {
     it("Should return the correct number of filtered projects based on name and description in lowercase", () => {
 
         const products : Product[] = [
-            { id: 1, name: "SUN GLASS", price: 10 },
-            { id: 2, name: "sun flower", price: 10 },
-            { id: 3, name: "Bag", price: 10 }
+            { id: 1, name: "SUN GLASS", price: 10 } as Product,
+            { id: 2, name: "sun flower", price: 10 } as Product,
+            { id: 3, name: "Bag", price: 10 } as Product
         ]
         const { filteredProducts, updateSearchTerm } = useProducts(products);
 
@@ -24,5 +24,35 @@ describe("useProducts", () => {
         updateSearchTerm('bag');
         expect(filteredProducts.value.length).toEqual(1);
     });
+
+    it("Should filter the products based on options", () => {
+        const products : Product[] = [
+            { id: 1, name: "Glass A", price: 10, options: ["metal", "white"] },
+            { id: 2, name: "Glass B", price: 20, options: ["plastic", "white"] },
+            { id: 3, name: "Glass C", price: 30, options: ["plastic", "green"] } 
+        ]
+
+        const { filteredProducts, updateOptions } = useProducts(products);
+        
+        expect(filteredProducts.value.length).toEqual(products.length);
+
+        updateOptions(["plastic"])
+        expect(filteredProducts.value.length).toEqual(2);
+        expect(filteredProducts.value).toEqual( [
+            { id: 2, name: "Glass B", price: 20, options: ["plastic", "white"] },
+            { id: 3, name: "Glass C", price: 30, options: ["plastic", "green"] } 
+        ]);
+
+
+        updateOptions(["metal"])
+        expect(filteredProducts.value.length).toEqual(1);
+        expect(filteredProducts.value).toEqual( [
+            { id: 1, name: "Glass A", price: 10, options: ["metal", "white"] }
+        ]);
+
+
+        updateOptions(["metal", "plastic"])
+        expect(filteredProducts.value.length).toEqual(3);
+    })
 
 })
